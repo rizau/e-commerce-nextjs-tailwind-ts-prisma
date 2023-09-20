513a078d016bff8771b4896bee1334696c74f999
Riza Uguroglu <riza.uguroglu@logo.com.tr>
2023-09-20 11:00:23 +0300

    clerk authentication added

--- admin/app/(root)/page.tsx
+++ admin/app/(root)/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
-  return <p>Hello Admin dashboard</p>;
-}
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default SetupPage;
