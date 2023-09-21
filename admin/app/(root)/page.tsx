f888c24691e40087ee58cb8426b3058ccc0ac1ea
Riza Uguroglu <riza.uguroglu@logo.com.tr>
2023-09-21 10:18:24 +0300

    prisma added for mssql

--- admin/app/(root)/page.tsx
+++ admin/app/(root)/page.tsx
@@ -1,22 +1,21 @@
-"use client";
-import { Modal } from "@/components/ui/modal";
-import { UserButton } from "@clerk/nextjs";
-import { useStoreModal } from "../../hooks/use-store-modal";
-import { useEffect } from "react";
+"use client"
+
+import { useEffect } from "react"
+import { UserButton } from "@clerk/nextjs"
+
+import { Modal } from "@/components/ui/modal"
+
+import { useStoreModal } from "../../hooks/use-store-modal"
 
 const SetupPage = () => {
-  const onOpen = useStoreModal((state)=>state.onOpen)
-  const isOpen = useStoreModal((state)=>state.isOpen)
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
 
-  useEffect(()=>{
-    if(!isOpen)onOpen()
-  },[isOpen,onOpen])
+  useEffect(() => {
+    if (!isOpen) onOpen()
+  }, [isOpen, onOpen])
 
-  return (
-    <div className="p-4">
-      Root page
-    </div>
-  );
-};
+  return <div className="p-4">Root page</div>
+}
 
-export default SetupPage;
+export default SetupPage
