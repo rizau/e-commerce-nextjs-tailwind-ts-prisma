0c1718767b226134fd62e193e40695157dbb6697
Riza Uguroglu <riza.uguroglu@logo.com.tr>
2023-09-21 08:59:41 +0300

    prettier setup fixed

--- admin/components/modals/store-modal.tsx
+++ admin/components/modals/store-modal.tsx
@@ -1,22 +1,30 @@
-"use client";
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 import * as z from "zod"
 
-import { useStoreModal } from "@/hooks/use-store-modal";
-import { Modal } from "@/components/ui/modal";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { useStoreModal } from "@/hooks/use-store-modal"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Modal } from "@/components/ui/modal"
 
 const formSchema = z.object({ name: z.string().min(1) })
 
 export const StoreModal = () => {
-  const storeModal = useStoreModal();
+  const storeModal = useStoreModal()
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: { name: "" }
+    defaultValues: { name: "" },
   })
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
@@ -31,18 +39,31 @@ export const StoreModal = () => {
     >
       <div>
         <div className="space-y-4 py-2 pb-4">
-        <Form {...form} >
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField control={form.control} name="name" render={({ field }) => (<FormItem><FormLabel>Name</FormLabel><FormControl><Input placeholder="store name" {...field} /></FormControl><FormMessage/></FormItem>)} />
-          <div className="pt-2 space-x-2 flex items-center justify-end">
-            <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-            <Button type="submit">Continue</Button>
-
-          </div>
-          </form>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="store name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-2 space-x-2 flex items-center justify-end">
+                <Button variant="outline" onClick={storeModal.onClose}>
+                  Cancel
+                </Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
           </Form>
         </div>
       </div>
     </Modal>
-  );
-};
+  )
+}
