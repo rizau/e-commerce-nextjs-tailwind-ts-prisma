836398c7828ab2dd87622037cc818c777e68c5fb
Riza Uguroglu <riza.uguroglu@logo.com.tr>
2023-09-21 10:49:13 +0300

    react-hot-toast added, store create form implement

--- admin/app/layout.tsx
+++ admin/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ModalProvider } from "@/providers/modal-provider"
+import { ToasterProvider } from "@/providers/toast-provider"
 import { ClerkProvider } from "@clerk/nextjs"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -21,6 +22,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
